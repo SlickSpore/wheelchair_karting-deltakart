ba9d7c86e446d061fdfdcc091821684898a83103
SlickSpore <ettorecaccioli@gmail.com>
2026-06-29 15:38:36 +0200

    sas

--- kart_controller_master/core/lib/kart_blind.py
+++ kart_controller_master/core/lib/kart_blind.py
@@ -6,7 +6,7 @@ import pandas as pd
 import math
 import time
 
-direction = 1.0
+direction = 0.0
 running = True
 lock = threading.Lock()
 
@@ -16,30 +16,30 @@ def get_panning(direction):
     theta = pan_val * (np.pi / 2.0)
     return np.cos(theta), np.sin(theta)
 
-def get_wave(direction, duration=0.1, amp=1, sample_rate=44100):
+def get_wave(direction, direction_pan_coef=1.7, duration=0.1, amp=1, sample_rate=44100):
 
     frequency = 1000 if direction > 0 else 2000
     frequency = frequency if direction != 0 else 800
 
     t_wave = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
 
-    l, r = get_panning(direction)
+    l, r = get_panning((1 if direction>0 else -1) * min(100, abs(direction*direction_pan_coef)))
 
-    if direction > 0:
+    if direction > 0.05:
         wave_right = amp * np.sin(2 * np.pi * 1500 * t_wave).astype(np.float32) 
         return np.column_stack((wave_right * l, wave_right * r))
-    elif direction < 0:
+    elif direction < -0.05:
         wave_left = amp * np.sin(2 * np.pi * 1000 * t_wave).astype(np.float32)   
         return np.column_stack((wave_left * l, wave_left * r))
-    elif direction == 0:
+    else:
         wave_zero = amp * np.sin(2 * np.pi * 800 * t_wave).astype(np.float32)  
         return np.column_stack((wave_zero, wave_zero))
 
-def play_audio():
+def beep_routine():
     global running, direction
     
     sample_rate = 44100
-    time_max = 1
+    time_max = 0.3
     last_beep = 0.0
 
     with sd.OutputStream(samplerate=sample_rate, channels=2) as stream:
@@ -49,7 +49,7 @@ def play_audio():
             with lock:
                 if not running:
                     break
-                current_interval = 0.5 * (1-abs(direction))
+                current_interval = time_max * (1-abs(direction))
 
             time_now = time.time()
             if time_now - last_beep >= current_interval:
@@ -61,14 +61,14 @@ def play_audio():
 def test_audio_indications():
     global direction, running
 
-    x = threading.Thread(target=play_audio)
+    x = threading.Thread(target=beep_routine)
     x.start()
 
     js = k_js.KartJoystickInput()
     try: 
         while True:
             js.trigger_update()
-            js.load_current_state(k_js.JS_SETUP, k_js.JS_DZ)
+            js.load_current_state(k_js.JS_SETUP, 1)
 
             steering = js.steering_angle if js.steering_angle != 0 else 10
             
@@ -97,14 +97,11 @@ def create_track():
         while True:
             js.trigger_update()
             js.load_current_state(k_js.JS_SETUP, k_js.JS_DZ)
-
-            print(js.steering_angle)
-
             js_positions.append(js.steering_angle)
 
             time.sleep(sample_rate) 
+
     except KeyboardInterrupt:
-        
         df = pd.DataFrame(np.array(js_positions))
         df.to_csv("core/lib/old/pista_esempio.csv")
 
@@ -113,21 +110,28 @@ def test_track():
 
     data = pd.read_csv("core/lib/old/pista_esempio.csv")
 
-    x = threading.Thread(target=play_audio)
+    positions = data.to_numpy(np.int32)[:, 1]
+
+    differences = []
+
+    x = threading.Thread(target=beep_routine)
     x.start()
 
     js = k_js.KartJoystickInput()
     try: 
-        for i in data.to_numpy(np.int32)[:, 1]:
+        for i in positions:
             js.trigger_update()
             js.load_current_state(k_js.JS_SETUP, k_js.JS_DZ)
 
-            steering =  ((js.steering_angle - i) if js.steering_angle - i  != 0 else 10)
+            steer_difference = js.steering_angle - i
             
-            print(i, js.steering_angle, steering)
+            differences.append(abs(steer_difference))
+
+            print(" "*150, end='\r')
+            print(f"Target: {i}\t Joystick: {js.steering_angle}\t Difference: {steer_difference}\t Precision: {100-np.round(np.mean(np.array(differences)),2)}", end="\r")
 
             with lock:
-                direction = (-1 if steering < 0 else 1) * (math.log10(abs(steering)/10))
+                direction = (steer_difference/100)
 
             time.sleep(0.02) 
 
@@ -139,5 +143,8 @@ def test_track():
         
     x.join()
 
+    print(f"\nScore: {100-np.round(np.mean(np.array(differences)), 2)}")
+
 if __name__ == "__main__":
     test_track()
+    # create_track()

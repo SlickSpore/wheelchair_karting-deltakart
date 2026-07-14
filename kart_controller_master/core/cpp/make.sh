b416796b7f4f0ec86dda870e56d1ad77f050839f
SlickSpore <ettorecaccioli@gmail.com>
2026-07-14 10:12:41 +0200

    cleanup

--- kart_controller_master/core/cpp/make.sh
+++
@@ -1,7 +0,0 @@
-python3 headset_run/setup.py build_ext --inplace
-
-rm -rf build/
-
-mv *.so headset_run/
-
-python3 main.py
--- kart_controller_master/core/tools/js_axes_viewer.py
+++
@@ -1,7 +0,0 @@
-import lib.kart_js as Joystick
-
-js = Joystick.KartJoystickInput()
-
-while True:
-    js.trigger_update()
-    print([js.get_specified_axes(i) for i in range(js.js.get_maxaxes())])
